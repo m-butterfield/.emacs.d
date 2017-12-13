4d85787dc47b42543b1808270a858d466aefffcb
Matt Butterfield <matt.butterfield@percolate.com>
2017-12-13 14:43:00 -0500

    projectile

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (projectile- fiplr magit evil ##)))
+ '(package-selected-packages (quote (go-mode projectile- fiplr magit evil ##)))
  '(whitespace-display-mappings
    (quote
     ((space-mark 32
